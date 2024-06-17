3e25fafed5c60beb64de3e6e6e38f2e0b43fec7e
DmitryByrigin <dmitri.d907@gmail.com>
2024-06-17 16:12:09 +0200

    Unused item deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@mantine/core/styles.css";
 import React from "react";
-import {MantineProvider, ColorSchemeScript, Center} from "@mantine/core";
+import {MantineProvider, ColorSchemeScript} from "@mantine/core";
 import { theme } from "../theme";
 
 export const metadata = {
